3be710e9b38c03117c0f2625972733457b9877c1
Harshpreet Kaur <114172812+harshpreet14@users.noreply.github.com>
2023-11-20 15:10:33 +0530

    changes to vercel.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config({path: '../.env'}); // Import and configure dotenv
 const app = require('./app'); // Import the Express application
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000; // Set the port
+//const PORT = process.env.PORT || 3000; // Set the port
 
 // MongoDB connection URL
 const MONGO_URI = process.env.MONGO_URI;
@@ -17,10 +17,12 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
         // Start the server only if MongoDB connection is successful
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+        app.listen(3000, () => {
+            console.log(`Server is running on port 3000`);
         });
     })
     .catch((err) => {
         console.error('Error connecting to MongoDB:', err.message);
     });
+
+module.exports = server;
